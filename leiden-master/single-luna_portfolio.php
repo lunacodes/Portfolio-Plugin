ff152a17f1982a6b912c0a631e9927e887cf0475
Luna Lunapiena <lunacodes@gmail.com>
2018-05-14 18:35:55 -0400

    deleted files fix

--- leiden-master/single-luna_portfolio.php
+++ leiden-master/single-luna_portfolio.php
@@ -9,14 +9,21 @@ function enqueue_portfolio_assets() {
     wp_enqueue_style( 'portfolio', get_stylesheet_directory_uri() . '/css/portfolio.css' );
 }
 
-wp_enqueue_script('handle_name', 'source', array('jquery'), 'version', false);
+// wp_enqueue_script('handle_name', 'source', array('jquery'), 'version', false);
+
 add_action( 'genesis_entry_content', 'display_project_content' );
-function display_project_content() { ?>
+function display_project_content() { 
+    $next_project = get_field(next_project_slug);
+    $next_project_url = get_site_url() . "/portfolio/" . $next_project;
+    ?>
 
     <div class="project-container">
         <div class="project-nav">
+            <a href="<?php echo($prev_project_url); ?>"><button class="project-nav-btn prev-project">&lt; Prev</button></a>
+            <a href="<?php echo($next_project_url); ?>"><button class="project-nav-btn next-project">Next &gt;</button></a>
             <!-- Look up how to do pagination for posts -->
         </div>
+
         <div class="project-wrapper">
             <div class="project-bg">  
     <?php if ( have_rows( 'project_img' ) ) : ?>
